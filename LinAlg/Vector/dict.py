2811265ff5809a17ac604bfb75b87834ff0dda9d
musichead42 <j6penguin@gmail.com>
2016-02-24 19:09:15 -0800

    Updated

--- LinAlg/Vector/dict.py
+++ LinAlg/Vector/dict.py
@@ -2,9 +2,26 @@ class Vec:
 	def __init__(self, labels, function):
 		self.D=labels
 		self.f=function  
-
-def zero_vec(D):
-	return Vec(D, {d:0 for d in D})
-x=zero_vec(('A', 'B', 'C'))
+	def setitem(self, d,val):
+		if d in self.D:
+			pass
+		else:
+			self.D.append(d)
+		self.f[d]=val
+	def getitem(self, d):
+		if d in self.f:
+			return self.f[d]
+		else:
+			return 0
+	def zero_vec(self):
+		for d in self.D:
+			self.f[d]=0
+def list2vec(L):
+	v=Vec(range(len(L)), {})
+	for i in range(len(L)):
+		v.f[i]=L[i]
+	return v
+x=Vec([],{})
+x.setitem('A',1)
+print x.getitem('A')
 print x.D
-print x.f
